71e3bce7427e81518b71c81b56f99e8da4750c5d
Spencer Davis <davissmd19@gmail.com>
2021-10-28 23:57:46 -0600

    Im stuck... code not working right now

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ import random
 # Machine's RGN (randomly generated number)
 machine = random.randrange(11)
 
+response = input("")
 # GAME
 def game ():
     print("Hello and welcome to my game! are you ready to play?")
-    response = input("")
+    #response = input("")
     if response == "yes":
         print("Lets begin!")
     else:
@@ -20,4 +21,11 @@ def game ():
     else:
         print("Nope, the number was:" + str(machine) )
 
+def re_run():
+    print("Want to play again?")
+    if response == "yes":
+        game()
+    else:
+        print("thanks for playing!")
+        exit()
 game()
